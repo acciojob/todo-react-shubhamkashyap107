768dccc92ee644ca7380774ad525e6639f2d7be1
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-08-28 10:25:38 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+
+  const[todos, setTodos] = useState([])
+  const[text, setText] = useState("")
+
+  function deleteBtnHandler(){
+
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        qwer567890plkjhgfdsxcvbnm
+        <input value={text} onChange={(e) => {
+          setText(e.target.value)
+        }} />
+        <button onClick={(e) => {
+          if(!text) return
+
+          const newTodo = {
+            task : text,
+            id : uuidv4()
+          }
+
+          setTodos([...todos, newTodo])
+
+        }}>Add Todo</button>
+
+        <ul>
+          {todos && todos.map((item) => {
+            return <ul key={item.id}>{item.task}  <button onClick={() => {
+              const narr = todos.filter((todo) => {
+                return todo.id != item.id
+              })
+
+              setTodos(narr)
+            }}>Delete</button></ul>
+          })}
+        </ul>
     </div>
   )
 }
